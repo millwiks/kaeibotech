8162d3f714fdba3766a819420573a4a1639d8ba8
kaeibo <102720149+kaeibo@users.noreply.github.com>
2022-09-23 13:41:56 +0300

    second commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -19,6 +19,13 @@
                 <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
             </div>
 
+            <!-- Username -->
+            <div class="mt-4">
+                <x-input-label for="username" :value="__('Username')" />
+
+                <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus />
+            </div>
+
             <!-- Email Address -->
             <div class="mt-4">
                 <x-input-label for="email" :value="__('Email')" />
