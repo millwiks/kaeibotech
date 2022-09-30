cc51ea6ee145616a922acaf7663e79e749a9a253
kaeibo <102720149+kaeibo@users.noreply.github.com>
2022-09-30 11:38:09 +0300

    4th commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
+    </head>
 <x-guest-layout>
+
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
@@ -54,3 +60,29 @@
         </form>
     </x-auth-card>
 </x-guest-layout>
+<body>
+       
+       <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+   
+   <script>
+    @if(Session::has('message'))
+    var type = "{{ Session::get('alert-type','info') }}"
+    switch(type){
+       case 'info':
+       toastr.info(" {{ Session::get('message') }} ");
+       break;
+       case 'success':
+       toastr.success(" {{ Session::get('message') }} ");
+       break;
+       case 'warning':
+       toastr.warning(" {{ Session::get('message') }} ");
+       break;
+       case 'error':
+       toastr.error(" {{ Session::get('message') }} ");
+       break; 
+    }
+    @endif 
+   </script> 
+       </body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\Demo\DemoController;
+use \App\Http\Controllers\AdminController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
+
+
+Route::controller(DemoController::class)->group(function(){
+    Route::get('/about','Index')->name('about.page');
+    Route::get('/contact','ContactMethod')->name('contact.page');
+});
+
+// admin all route
+Route::controller(AdminController::class)->group(function(){
+    Route::get('/admin/logout','destroy')->name('admin.logout');
+    Route::get('/admin/profile','profile')->name('admin.profile');
+    Route::get('/edit/profile', 'EditProfile')->name('edit.profile');
+    Route::post('/store/profile', 'StoreProfile')->name('store.profile');
+
+    Route::get('/change/password', 'ChangePassword')->name('change.password');
+    Route::post('/update/password', 'UpdatePassword')->name('update.password');
+    
+  
+});
+
 
 Route::get('/', function () {
     return view('welcome');
