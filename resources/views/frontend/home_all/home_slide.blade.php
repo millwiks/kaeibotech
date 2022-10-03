b37166ba925ed84d9a756622404ed92efffb1c48
kaeibo <102720149+kaeibo@users.noreply.github.com>
2022-10-03 12:39:38 +0300

    7B commit

--- resources/views/frontend/home_all/home_slide.blade.php
+++ resources/views/frontend/home_all/home_slide.blade.php
@@ -5,25 +5,33 @@ $homeslide = App\Models\HomeSlide::find(1);
 <!-- banner2 -->
  <div class="three_box">
          <div class="container">
+         <div class="row">
+               <div class="col-md-12">
+                  <div class="titlepage">
+                     <h2>SHOP NOW BELOW</h2>
+                  </div>
+               </div>
+            </div>
             <div class="row">
                <div class="col-md-4">
                   <div class="box_text">
-                     <i><img src="{{ asset('frontend/assets/images/thr.png')}}" alt="#"/></i>
-                     <h3>Computer</h3>
+                  
+                     <i><a href="product.html"><img src="{{ asset('frontend/assets/images/thr.png')}}" alt="#"/></a></i>
+                     <h3>High Voltage Cables</h3>
                      <p>{{ $homeslide->description1 }} </p>
                   </div>
                </div>
                <div class="col-md-4">
                   <div class="box_text">
-                     <i><img src="{{ asset('frontend/assets/images/thr1.png')}}" alt="#"/></i>
-                     <h3>Laptop</h3>
+                     <i><a href="product.html"><img src="{{ asset('frontend/assets/images/thr1.png')}}" alt="#"/></a></i>
+                     <h3>Power Switches</h3>
                      <p>{{ $homeslide->description2 }} </p>
                   </div>
                </div>
                <div class="col-md-4">
                   <div class="box_text">
-                     <i><img src="{{ asset('frontend/assets/images/thr2.png')}}" alt="#"/></i>
-                     <h3>Tablet</h3>
+                     <i><a href="product.html"><img src="{{ asset('frontend/assets/images/thr2.png')}}" alt="#"/></a></i>
+                     <h3>Lighting Bulbs</h3>
                      <p>{{ $homeslide->description3 }} </p>
                   </div>
                </div>
